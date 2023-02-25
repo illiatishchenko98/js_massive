f761a026b57bfa9edac428a4c8fd48027886bba7
illiatishchenko98 <102582375+illiatishchenko98@users.noreply.github.com>
2023-02-24 21:30:59 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let arr = [10, 22, 21, 6, 8];
+arr.sort();
+console.log(arr);
+
+function sortNum(a, b) {
+    return a - b;
+}
+
+// // arr.pop();
+
+// arr.push(10);
+
+// console.log(arr);
+
+
+// for (i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+// })
+
+// let str = prompt("", "");
+// let products = str.split(", ");
+// products.sort();
+// console.log(products.join("; "));
